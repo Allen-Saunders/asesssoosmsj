cea172b8aee27cf400b240aee60d6979fdeb5b63
kronic5179 <98897386+Allen-Saunders@users.noreply.github.com>
2022-12-14 18:46:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import random
+
+# "secret code"
+print(random.randint(0,9))
+print(random.randint(0,9))
+print(random.randint(0,9))
+print(random.randint(0,9)) 
+
+# asking for numbers
+print("Select 4 numbers between 0 and 9")
+
+# how many they got right
+
+if line 4 
